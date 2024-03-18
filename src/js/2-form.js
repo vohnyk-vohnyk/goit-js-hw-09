3bb87f92c52e68ae529191b9eb0784117ba7e4f7
vohnyk-vohnyk <132850694+vohnyk-vohnyk@users.noreply.github.com>
2024-03-18 20:59:04 +0200

    fix form

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,31 +1,36 @@
 const form = document.querySelector(".feedback-form");
 
 function saveMessageInLocalStorage(event) {
-    const inputForm = {};
-    const email = event.currentTarget.email.value;
-    const message = event.currentTarget.message.value;
-    inputForm.email = email.trim();
-    inputForm.message = message.trim();
+  const inputForm = {};
+  const email = event.currentTarget.email.value.trim();
+  const message = event.currentTarget.message.value.trim();
+
+  // Перевірка на наявність корисних даних
+  if (email !== "" && message !== "") {
+    inputForm.email = email;
+    inputForm.message = message;
     localStorage.setItem("feedback-form-state", JSON.stringify(inputForm));
+  }
 }
 
-
 if (localStorage.getItem("feedback-form-state")) {
-    const saveMessage = JSON.parse(localStorage.getItem("feedback-form-state"));
-    form.email.value = saveMessage.email;
-    form.message.value = saveMessage.message;
+  const saveMessage = JSON.parse(localStorage.getItem("feedback-form-state"));
+  form.email.value = saveMessage.email;
+  form.message.value = saveMessage.message;
 }
 
 form.addEventListener("input", saveMessageInLocalStorage);
 
 function submitMessage(event) {
-    event.preventDefault();
-    if (form.email.value !== "" && form.message.value !== "") {
-        console.log(JSON.parse(localStorage.getItem("feedback-form-state")));
-        localStorage.removeItem("feedback-form-state");
-        form.reset();
-    }
+  event.preventDefault();
+  const email = form.email.value.trim();
+  const message = form.message.value.trim();
+
+  if (email !== "" && message !== "") {
+    console.log(JSON.parse(localStorage.getItem("feedback-form-state")));
+    localStorage.removeItem("feedback-form-state");
+    form.reset();
+  }
 }
 
 form.addEventListener("submit", submitMessage);
-
